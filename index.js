ecef974b80f58f4340a6d0b0a405b932e45df9fa
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-01-01 18:28:41 +0100

    emulator thing done prob

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { keyboard, key } from '@nut-tree/nut-js'
+import { keyboard, Key } from '@nut-tree/nut-js'
 import { execa } from 'execa'
 import 'dotenv/config'
 import { Client } from 'discord.js'
@@ -11,10 +11,10 @@ const client = new Client({
 	]
 })
 
-client.on('ready', () => {
+client.on('ready', async () => {
 	console.log(colorette.bgGreen('Logged onto Discord!'))
-
-	execa('echo', 'hey').then(process => console.log(colorette.bgBlue(process.stdout)))
+	console.log(colorette.bgBlue('Starting up the emulator'))
+	execa('mednafen', './roms/pokegold.gbc')
 })
 
 client.login(process.env.TOKEN)
