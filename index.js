fb9b6d6879447d190cfac810c78852be32d9690d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-01-01 18:36:13 +0100

    log process stdout

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ client.on('ready', async () => {
 	console.log(colorette.bgGreen('Logged onto Discord!'))
 
 	console.log(colorette.bgBlue('Starting up the emulator'))
-	execa('mednafen', 'roms/pokegold.gbc')
+	execa('mednafen', 'roms/pokegold.gbc').then(process => console.log(process.stdout))
 })
 
 client.login(process.env.TOKEN)
