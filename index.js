b1ea593beab10fa536746f132bc8352113635abd
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2023-01-01 18:54:04 +0100

    please work thx

--- index.js
+++ index.js
@@ -12,9 +12,31 @@ const client = new Client({
 
 client.on('ready', async () => {
 	console.log(colorette.bgGreen('Logged onto Discord!'))
+})
 
+client.on('ready', async () => {
 	console.log(colorette.bgBlue('Starting up the emulator'))
-	execa('mednafen', ['./roms/pokegold.gbc'], {shell: true}).then(process => console.log(process.command))
+	execa('mednafen', ['./roms/pokegold.gbc'], {shell: true})
+})
+
+client.on('ready', async () => {
+	console.log(colorette.bgBlue('Streaming to Twitch!'))
+	execa('ffmpeg', [
+		'-f', 'x11grab',
+		'-s', '854x480',
+		'-i', `${execa('echo', ['$DISPLAY']).then(cmd => cmd.stdout)}+0,0`,
+		'-vcodec', 'libx264',
+		'-preset', 'ultrafast',
+		'-tune', 'zerolatency',
+		'-r', '30',
+		'-b:v', '2500k',
+		'-bufsize', '5000k',
+		'-acodec', 'libmp3lame',
+		'-ar', '44100',
+		'-b:a', '128k',
+		'-f', 'flv',
+		`rtmp://mad02.contribute.live-video.net/app/${process.env.TWITCH_STREAMKEY}`
+	  ], {shell: true})
 })
 
 client.login(process.env.TOKEN)
